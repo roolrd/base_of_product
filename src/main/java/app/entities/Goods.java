b98004f233d81ab4c2270404cf8458365228c3e4
roolrd <ruslanriznyk@gmail.com>
2019-08-12 14:54:05 +0300

    Працюють чорнові сервлети, створюю Клас "товари".

---
+++ src/main/java/app/entities/Goods.java
@@ -0,0 +1,18 @@
+package app.entities;
+
+import java.util.GregorianCalendar;
+
+public class Goods {
+    private int id;
+    private String name;
+    private String category;
+    private GregorianCalendar dateOfProduce;
+    private String dateOfExpired;
+    private int amount;
+    private double price;
+    private String provider;
+    private String nPhoneProvider;
+    
+
+
+}
--- src/main/java/app/servlets/ReportServlet.java
+++ src/main/java/app/servlets/ReportServlet.java
@@ -1,19 +1,22 @@
 package app.servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ReportServlet")
 public class ReportServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/report.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
