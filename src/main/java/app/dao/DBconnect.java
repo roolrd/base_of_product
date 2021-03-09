8e72a54e8fba6f709f24370a70bd5fa2cbc8c608
Ruslan Riznyk <44848472+roolrd@users.noreply.github.com>
2021-03-09 07:27:05 +0200

    rds address

--- src/main/java/app/dao/DBconnect.java
+++ src/main/java/app/dao/DBconnect.java
@@ -13,7 +13,7 @@ public class DBconnect {
 
         if (connection == null) {try {
                         Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://52.59.231.162:3306/base_products?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "rO,75olRD");
+            connection = DriverManager.getConnection("jdbc:mysql://base-products.cpjkpr5piyap.eu-central-1.rds.amazonaws.com:3306/base_products?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root", "rO,75olRD");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
